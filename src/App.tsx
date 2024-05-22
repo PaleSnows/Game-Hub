3084537ba96397bcac32e8e340c6ea756c6b103a
Renzaki <renzakithakuri@gmail.com>
2024-05-22 07:48:20 +0545

    adding sorting functionality to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,54 @@
-import { Grid, GridItem, Show,  } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
+import SortSelector from "./components/SortSelector";
 
-
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+  sortOrder:string
+}
 
 function App() {
   // Initially no genre sleected so null
-  const [selectedGenre,setSelectedGenre] = useState<Genre | null>(null)
+  // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
   return (
     <Grid
       templateAreas={{ base: `"nav " "main"`, lg: `"nav nav" "aside main"` }}
-      templateColumns={{base:"1fr",lg:"200px 1fr"}}
+      templateColumns={{ base: "1fr", lg: "200px 1fr" }}
     >
-      <GridItem area={"nav"} >
+      <GridItem area={"nav"}>
         <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} paddingX={5}>
-          <GenreList onSelectGenre={(genre)=>setSelectedGenre(genre)} selectedGenre={selectedGenre}/>
+          <GenreList
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+            selectedGenre={gameQuery.genre}
+          />
         </GridItem>
       </Show>
-      <GridItem area={"main"} >
-        <GameGrid selectedGenre={selectedGenre}/>
+      <GridItem area={"main"} paddingLeft={2} marginBottom={5}>
+        <HStack spacing={5}>
+          <PlatformSelector
+            selectPlatform={gameQuery.platform}
+            onSelectPlatform={(platform) =>
+              setGameQuery({ ...gameQuery, platform })
+            }
+          />
+          <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=>setGameQuery({...gameQuery,sortOrder})}/>
+        </HStack>
+
+        <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
   );
---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,34 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatform";
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectPlatform: Platform | null;
+}
+
+
+const PlatformSelector = ({ onSelectPlatform,selectPlatform }: Props) => {
+  const { data, error } = usePlatforms();
+  if (error) return null;
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectPlatform?.name || "Platforms"}
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}{" "}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
